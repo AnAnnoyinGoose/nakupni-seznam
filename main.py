82a56b6fb45e35f44fe8d735f14601cbd7aa2ac7
AnAnnoyinGoose <97093654+AnAnnoyinGoose@users.noreply.github.com>
2024-09-23 10:11:54 +0200

    List a help metoda.

--- main.py
+++ main.py
@@ -3,10 +3,25 @@ class Seznam:
   def add(self, inp):
     self.data.append(inp)
   def rem(self, inp):
-        try:
-          self.data.remove(inp)
-        except Exception as E:
-          print(E)
+    try:
+      self.data.remove(inp)
+    except Exception as E:
+      print(E)
+  def list(self):
+    if self.data:
+      for idx, e in enumerate(self.data, 1):
+        print(f"{idx}. - {e}")
+    else: 
+      print("Seznam je prázdný.")
+  def help(self):
+    print("1) Help msg.")
+    print("2) Add element.")
+    print("3) Remove element.")
+    print("4) List all elements.")
+    print("5) Exit program.")
+
+
+
 s = Seznam()
 
 def main():
